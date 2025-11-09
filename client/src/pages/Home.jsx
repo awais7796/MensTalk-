9462ad54291cb02a3bb58870753fe2b44de0981a
awais7796 <awaitzz49@gmail.com>
2025-11-09 21:47:00 +0530

    styled homw page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,19 +1,27 @@
 import Button from "../components/Button";
 import React from "react";
+import Pattern from "../components/Pattern";
+
 const Home = () => {
   return (
     <>
-      <div className="flex flex-col h-screen justify-center items-center ">
-        <h1>MansTalk </h1>
-        <h3>
-          <p>
-            We talk about the stuff group chats pretend they don’t talk about.
+      <Pattern>
+        <div className="flex flex-col h-screen justify-center items-center bg-[#0F0E0D] text-[#E8DCC4] px-4 text-center">
+          <h1 className="text-5xl font-semibold tracking-wide mb-4 text-[#E8DCC4]">
+            MansTalk
+          </h1>
+
+          <p className="text-lg max-w-xl mb-2 text-[#CFC4AA] leading-relaxed">
+            We talk about the conversations men keep hidden behind the silence.
           </p>
 
-          <h4>No judgement. No cringe. Just straight answers.</h4>
-        </h3>
-        <Button label="lets discuss" />
-      </div>
+          <h4 className="text-md max-w-md mb-10 text-[#AFA48D]">
+            No judgement. No cringe. Just straight truth.
+          </h4>
+
+          <Button label="Let's Discuss" />
+        </div>
+      </Pattern>
     </>
   );
 };
